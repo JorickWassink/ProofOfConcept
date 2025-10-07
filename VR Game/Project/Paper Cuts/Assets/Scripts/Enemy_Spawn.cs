1be8bbad4b4c470c5ea8f5317cc7711c018c376f
RubenStender <152160273+RubenStender@users.noreply.github.com>
2025-10-07 11:56:37 +0200

    Enemy spawn werkt
    Enemy spawned nu 3 verschillende type enemies

---
+++ VR Game/Project/Paper Cuts/Assets/Scripts/Enemy_Spawn.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class Enemy_Spawn : MonoBehaviour
+{
+    [Header("Enemy Prefabs")]
+    public GameObject[] enemyPrefab; // Enemy prefab
+
+    [Header("Spawn Points")]
+    public Transform[] spawnPoints; // Alle spawnpoints
+
+    [Header("Spawn Settings")]
+    public float spawnInterval = 3f; // Hoe vaak gespawned moet worden
+    private float timer;
+
+    void Start()
+    {
+        timer = spawnInterval;
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0f)
+        {
+            SpawnEnemy();
+            timer = spawnInterval; // Reset timer
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        if (spawnPoints.Length == 0 || enemyPrefab.Length == 0)
+            return;
+
+        Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        GameObject prefabToSpawn = enemyPrefab[Random.Range(0, enemyPrefab.Length)];
+
+        Instantiate(prefabToSpawn, spawnPoint.position, spawnPoint.rotation);
+    }
+}
