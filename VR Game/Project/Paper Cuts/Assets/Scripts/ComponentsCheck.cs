f98cee817c8c6c484be95e51ed959ef47db8dd4c
Daniël Kosters <0356076@student.rocvantwente.nl>
2025-10-08 13:58:35 +0200

    Style
    mijn code beschrijvende commends gegeven en code leesbaarder gemaakt

--- VR Game/Project/Paper Cuts/Assets/Scripts/ComponentsCheck.cs
+++ VR Game/Project/Paper Cuts/Assets/Scripts/ComponentsCheck.cs
@@ -3,5 +3,5 @@ using UnityEngine;
 static class ComponentsCheck
 {
     //https://stackoverflow.com/questions/35166730/how-to-check-if-a-game-object-has-a-component-method-in-unity
-    public static bool HasComponent<T>(this GameObject obj) => obj.GetComponent<T>() != null;
+    public static bool HasComponent<T>(this GameObject obj) => obj.GetComponent<T>() != null;//Er word gekeken of een mee gegeven component (<T>) in de mee gegeven gameObject zit
 }
