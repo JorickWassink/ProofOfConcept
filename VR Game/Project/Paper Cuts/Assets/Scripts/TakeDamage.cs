48d418c19afd5f76907eb4ef2a913f973dd9b9c3
Jorick Wassink <wassinkjorick@gmail.com>
2025-10-09 14:10:29 +0100

    feat: put more things into one scene

--- VR Game/Project/Paper Cuts/Assets/Scripts/TakeDamage.cs
+++ VR Game/Project/Paper Cuts/Assets/Scripts/TakeDamage.cs
@@ -3,14 +3,21 @@ using UnityEngine;
 public class TakeDamage : MonoBehaviour
 {
     [SerializeField] GameObject endScreen;
-    private void OnCollisionEnter(Collision collision)
+    EnemyWavesSystem eWS;
+
+    private void Start()
+    {
+        eWS = FindAnyObjectByType<EnemyWavesSystem>();
+    }
+    private void OnTriggerEnter(Collider collision)
     {
         if (ComponentsCheck.HasComponent<NavMeshMovement>(collision.gameObject))
         {
             if (EventManager.CheckPlayerDeath())
             {
                 EventManager.DamageTaken();
-                collision.gameObject.SetActive(false);
+                Destroy(collision.gameObject);
+                eWS.EnemyDied();
             }
             else
             {
