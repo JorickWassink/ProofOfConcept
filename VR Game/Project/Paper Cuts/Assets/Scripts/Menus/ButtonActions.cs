ef2a097388dbfbf513805c8e1737d4aeb93f0914
Fleur Spannenberg <fleur.spannenberg@gmail.com>
2025-10-09 14:45:07 +0200

    Added pause menu

--- VR Game/Project/Paper Cuts/Assets/Scripts/Menus/ButtonActions.cs
+++ VR Game/Project/Paper Cuts/Assets/Scripts/Menus/ButtonActions.cs
@@ -1,9 +1,13 @@
+using TMPro.EditorUtilities;
 using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class ButtonActions : MonoBehaviour
 {
+    [SerializeField] GameObject menus;
+
     public void QuitGame()
     {
         Application.Quit();
@@ -13,5 +17,25 @@ public class ButtonActions : MonoBehaviour
     public void StartGame()
     {
         SceneManager.LoadScene("Game");
+    }
+
+    public void ReturnToMain()
+    {
+        SceneManager.LoadScene("MainMenu_Scene");
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        menus.SetActive(true);
+    }
+
+    public void PauseGame(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Time.timeScale = 0f;
+            menus.SetActive(true);
+        }
     }
 }
