01f2653dcce20b3681e8c0088429ebf9e3e97147
Jorick Wassink <wassinkjorick@gmail.com>
2025-10-13 13:17:30 +0100

    feat: cameramovement
    you can now move the camera

---
+++ Turn Based Game/Project/BootsAndBows/Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraZoom : MonoBehaviour
+{
+    Camera cam;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetZoomInValue(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void Get
+}
