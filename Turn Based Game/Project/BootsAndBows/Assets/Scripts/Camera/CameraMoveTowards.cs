30514fc92989508d2c265c8d5ce5ff1933fe57eb
Jorick Wassink <wassinkjorick@gmail.com>
2025-10-14 13:59:42 +0100

    feat: CameraMoveTowards script almost finished
    no moves towards the target object smoothly
    
    still need to add the check to delete itself after it is done moving

---
+++ Turn Based Game/Project/BootsAndBows/Assets/Scripts/Camera/CameraMoveTowards.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class CameraMoveTowards : MonoBehaviour
+{
+    //PLACE SCRIPT NOT ON THE OBJECT YOU WANT TO MOVE TOWARDS BUT PLACE IT ON A CHILD OBJECT WHOSE ONLY PURPOSE IS TO HOLD THE SCRIPT
+    Rigidbody2D rb;
+    Camera cam;
+    float speed = 2;
+    Transform target;
+    void Start()
+    {
+        cam = FindAnyObjectByType<Camera>();
+        rb = cam.gameObject.GetComponent<Rigidbody2D>();
+        target = gameObject.transform;
+        
+    }
+
+    private void Update()
+    { 
+        target.position = new Vector3 (target.position.x,target.position.y,cam.transform.position.z);
+        cam.transform.position = Vector3.Lerp(cam.transform.position, target.transform.position, Time.deltaTime * speed);
+    }
+}
